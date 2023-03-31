3ee1f66d19dfbd51bc8e3c2a545e95e6d836f9e0
Fakhri abdillah <fakriggwp@gmail.com>
2023-03-31 11:05:59 +0700

    array and number

--- quicksortt_107/quicksortt_107.cpp
+++ quicksortt_107/quicksortt_107.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];				//array of integres to hold values
+int cmp_count = 0;			//number of comparison
+int mov_count = 0;			//number of data movements
+int n;
